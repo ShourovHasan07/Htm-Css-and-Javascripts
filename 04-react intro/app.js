96589deab6b17a5e2f1baf80f125d391188ac155
shourovhasan~ <shourovhasan001@gmail@.com>
2024-01-02 19:33:23 +0600

    recat first part

---
+++ 04-react intro/app.js
@@ -0,0 +1,21 @@
+
+
+
+    import React from "react"
+    import reactDom from " react-dom"
+
+    const navbar=(
+<nav>
+    <h1> resturant 
+       </h1>
+    <ul>
+        <li> manu</li>
+        <li>service </li>
+        <li> contac</li>
+    </ul>
+    </nav>
+       
+        
+    )
+    
+    ReactDom.render(navbar,document.getElementById("root"))
